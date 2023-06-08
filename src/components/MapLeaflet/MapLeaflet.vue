9725dbefb008c866e36e5fd454c4881b5e431dcc
Daniel Correa <daniel.correa@easesolutions.com>
2023-06-08 11:48:49 -0500

    Add color to markers

--- src/components/MapLeaflet/MapLeaflet.vue
+++ src/components/MapLeaflet/MapLeaflet.vue
@@ -4,8 +4,10 @@
       <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" layer-type="base"
         name="OpenStreetMap"></l-tile-layer>
 
-      <l-marker v-for="(data, index) in coordinates" :key="index" :lat-lng="data.coordinates"></l-marker>
-      <l-circle v-for="(data, index) in coordinates" :key="index" :lat-lng="data.coordinates" :radius="800 * 800" :color="data.color"></l-circle>
+      <l-marker v-for="(data, index) in coordinates" :key="index" :lat-lng="data.coordinates"
+        :icon="getMarkerIcon(data.color)"></l-marker>
+      <l-circle v-for="(data, index) in coordinates" :key="index" :lat-lng="data.coordinates" :radius="800 * 800"
+        :color="data.color"></l-circle>
     </l-map>
   </div>
 </template>
@@ -14,9 +16,10 @@
 import { countryCodes } from "@src/data/data";
 import type { CountryCode } from "@src/types/CountryCode";
 import { LCircle, LMap, LMarker, LTileLayer } from "@vue-leaflet/vue-leaflet";
-import type { LatLngTuple } from "leaflet";
 import "./MapLeaflet.css";
 import { countryData } from "./countryData";
+import type { LatLngTuple } from "leaflet";
+import L from "leaflet";
 
 export default {
   components: {
@@ -41,7 +44,16 @@ export default {
         }
         return [];
       });
-    }
+    },
+    getMarkerIcon(color: string) {
+      return L.icon({
+        iconUrl: `https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-${color}.png`,
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41],
+      });
+    },
   },
 };
 </script>
--- src/components/MapLeaflet/countryData.ts
+++ src/components/MapLeaflet/countryData.ts
@@ -4,7 +4,7 @@ import type { LatLngTuple } from "leaflet";
 const { Colombia, Germany, Singapore, UnitedStates } = CountryCode;
 
 export const countryData: { [key in CountryCode]: { coordinates: LatLngTuple; color: string } } = {
-  [Colombia]: { coordinates: [4.5709, -74.2973], color: "#FFBF00" },
+  [Colombia]: { coordinates: [4.5709, -74.2973], color: "yellow" },
   [Singapore]: { coordinates: [1.3521, 103.8198], color: "green" },
   [Germany]: { coordinates: [51.1657, 10.4515], color: "red" },
   [UnitedStates]: { coordinates: [37.0902, -95.7129], color: "blue" },
